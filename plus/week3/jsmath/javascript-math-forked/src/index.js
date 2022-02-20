c1f5579aa38e9e97738af7f8edfcbc44c62ad0da
Valentina Muñoz <vmunoz@acl.cl>
2022-02-20 02:14:56 -0300

    JS math

---
+++ plus/week3/jsmath/javascript-math-forked/src/index.js
@@ -0,0 +1,15 @@
+// Challenge 1
+let price = 4.5;
+console.log(Math.round(price));
+
+// Challenge 2
+console.log(Math.ceil(price));
+
+// Challenge 3
+console.log(Math.floor(price));
+
+// Challenge 4
+console.log(Math.min(4, 1, 3, 10));
+
+// Challenge 5
+console.log(Math.max(4, 1, 3, 10));
