0d964db9732a94f3f2f26f374dee28a5da1f0064
Valentina Muñoz <vmunoz@acl.cl>
2022-02-20 17:31:44 -0300

    add js objects

---
+++ plus/week3/jsobjects/javascript-objects-forked/src/index.js
@@ -0,0 +1,56 @@
+// Challenge 1
+let weather = [
+  {},
+  {
+    temp: 22.5,
+    humidity: "20%"
+  },
+  {
+    windspeed: "11 km/h"
+  }
+];
+
+console.log(weather[0]);
+
+// Challenge 2
+console.log(weather[1]);
+
+// Challenge 3
+console.log(weather[1].temp);
+console.log(weather[1].humidity);
+
+// Challenge 4
+console.log(weather[2].windspeed);
+
+// Challenge 5
+let forecast = [
+  {
+    day: "Monday",
+    temperature: 11.5
+  },
+  {
+    day: "Tuesday",
+    temperature: 21.5
+  },
+  {
+    day: "Wednesday",
+    temperature: 20.0
+  },
+  {
+    day: "Thursday",
+    temperature: 25.5
+  },
+  {
+    day: "Friday",
+    temperature: 18.0
+  },
+  {
+    day: "Saturday",
+    temperature: 20.0
+  },
+  {
+    day: "Sunday",
+    temperature: 23.0
+  }
+];
+console.log(forecast);
---
+++ plus/week3/jsobjects/javascript-objects-solutions/src/index.js
@@ -0,0 +1,44 @@
+// Challenge 1 and 2
+let weather = {
+  temp: 20,
+  humidity: 90
+};
+console.log(weather);
+
+// Challenge 3
+console.log(weather.temp);
+console.log(weather.humidity);
+
+// Challenge 4
+weather.windSpeed = 3.4;
+console.log(weather.windSpeed);
+
+// Challenge 5
+console.log(weather["windSpeed"]);
+console.log(weather["temp"]);
+console.log(weather["humidity"]);
+
+// Challenge 6
+let forecast = [
+  {
+    day: "Monday",
+    temp: 25
+  },
+  {
+    day: "Tuesday",
+    temp: 26
+  },
+  {
+    day: "Wednesday",
+    temp: 30
+  },
+  {
+    day: "Thursday",
+    temp: 14
+  },
+  {
+    day: "Friday",
+    temp: 22
+  }
+];
+console.log(forecast);
