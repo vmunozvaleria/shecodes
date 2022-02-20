21e849db13be8ea91c8b884165e3174e8577df5c
Valentina Muñoz <vmunoz@acl.cl>
2022-02-20 00:57:22 -0300

    JS strings

---
+++ plus/week3/jsstrings/javascript-strings-forked/src/index.js
@@ -0,0 +1,25 @@
+// JS Challenge 1
+
+let country = "Guinea Bissau";
+country = country.replace(" ", "-");
+console.log(country);
+
+// JS Challenge 2
+
+let city = " Sydney   ";
+city = city.trim();
+console.log(city);
+
+// JS Challenge 3
+let attraction = "Opera House";
+attraction = attraction.toLowerCase();
+console.log(attraction);
+
+// JS Challenge 4
+let place = "School";
+place = place.toUpperCase();
+console.log(place);
+
+// JS Challenge 5
+let sentence = `I went to visit the ${attraction} in ${city}, right next to my ${place.toLowerCase()}. Now, I'm getting ready for my trip to ${country}`;
+console.log(sentence);
---
+++ plus/week3/jsstrings/javascript-strings-solution/src/index.js
@@ -0,0 +1,17 @@
+let country = "Guinea Bissau";
+
+console.log(country.replace(" ", "-"));
+
+let city = " Sydney   ";
+city = city.trim();
+console.log(city);
+
+let attraction = "Opera House";
+console.log(attraction.toLowerCase());
+
+let place = "School";
+console.log(place.toUpperCase());
+
+let sentence = `I went to visit the ${attraction} in ${city}, right next to my ${place.toLowerCase()}. Now, I'm getting ready for my trip to ${country}!`;
+
+console.log(sentence);
