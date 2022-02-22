5603e86eed53d9c73b782eb18e44baaa75b87afb
Valentina Muñoz <vmunoz@acl.cl>
2022-02-21 22:56:37 -0300

    add js functions

---
+++ plus/week4/jsfunctions/js-functions-forked/src/index.js
@@ -0,0 +1,14 @@
+// Challenge
+// 1. Create a function isWindy and alert 'It is windy'
+// 2. Call the function twice
+function isWindy() {
+  // numbers to express KM/H
+  let wind = 21;
+  if (wind > 20) {
+    alert("It is windy");
+  } else {
+    alert("It is a calm day");
+  }
+}
+isWindy();
+isWindy();
---
+++ plus/week4/jsfunctions/js-functions-solutions/src/index.js
@@ -0,0 +1,10 @@
+// Challenge 1
+// 1. Create a function isWindy and alert 'It is windy'
+// 2. Call the function twice
+
+function isWindy() {
+  alert("It is windy");
+}
+
+isWindy();
+isWindy();
